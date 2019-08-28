e3e1254c6b6b8f644a28dd74e59afe43c9c6d8a7
Dat Nguyen <kcboi95@gmail.com>
2019-08-28 21:45:49 +0700

    Remove PHP5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,8 +2,9 @@
 
 ## Unreleased
 - Plugin support
+- Command line tool for global system
 
-## [1.0.0](#1.0.0)
+## [1.0.0](#100)
 Released on 2019-08-06
 ### Added
 - Adminer image
@@ -14,12 +15,20 @@ Released on 2019-08-06
 - Auto generate `docker-compose.yml` and `Dockerfile` 
 - Support user preferences configs with `myserver.ini`
 
-## [1.0.1](#1.0.1)
+## [1.0.1](#101)
 Released on 2019-08-17
 ### Fixed
-- Dockerfile not auto generated
+- `Dockerfile` not auto generated
 - Auto-build images not working
 - Remove `--compress` option when build docker images
 
 ### Changed
 - Change `./server` to `./myserver` in `README.md`
+
+## [1.1.0](#110)
+Released on 2019-08-28
+### Changed
+- `index.php` & `index.html` content
+
+### Removed
+- PHP5
--- htdocs/index.php
+++ htdocs/index.php
@@ -1,12 +1,39 @@
-<?php
-//phpinfo();die;
-$files = glob('*');
-$cwd = __DIR__;
-foreach ($files as $k => $file) {
-    $path = $cwd . DIRECTORY_SEPARATOR . $file;
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to MyServer</title>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <style>
+        * {
+            font-size: 13px;
+            margin: 0;
+            padding: 0;
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+        }
 
-    if (is_dir($path)) {
-        echo "<a href='/$file'>$file</a><br />";
-    }
+        body {
+            width: 100%;
+            height: 100vh;
+            background: #f0f0f0;
+        }
 
-}
+        body > p {
+            display: block;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+
+            font-family: Roboto, "Courier New", Arial, Helvetica, sans-serif;
+            font-size: 2rem;
+            color: #333333;
+        }
+    </style>
+</head>
+<body>
+<p>It's works!</p>
+</body>
+</html>
